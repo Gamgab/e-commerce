700b8cd67fe494cfa6593617e8d211074cecdede
Shinpoko <orduresol@gmail.com>
2023-04-24 15:32:09 +0200

    api railway

--- src/slices/api.js
+++ src/slices/api.js
@@ -1,5 +1,6 @@
-export const url = "https://shop-api-4i7l.onrender.com/api";
+/*export const url = "https://shop-api-4i7l.onrender.com/api";*/
 /*export const url = "http://localhost:5000/api";*/
+export const url = "https://web-production-c3be.up.railway.app/api";
 
 // ajout du token dans le header de la requete
 export const setHeaders = () => {
